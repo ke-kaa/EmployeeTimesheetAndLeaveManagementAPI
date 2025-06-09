d237f38b7a516a643cf65d71cec37ea0a5a49010
ke-kaa <amsalukaku122@gmail.com>
2025-06-10 00:11:53 +0300

    Create CustomUser model with role field

--- api_authentication/models.py
+++ api_authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class CustomUser(User):
+    ROLE_CHOICES = (
+        ("EMPLOYEE", "EMPLOYEE"),
+        ("MANAGER", "MANAGER"),
+        ("ADMIN", "ADMIN"),
+    )
+
+    roles = models.CharField(choices=ROLE_CHOICES, default=ROLE_CHOICES[0])
