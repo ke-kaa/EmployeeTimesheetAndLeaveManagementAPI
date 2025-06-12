1190042fd2ee34b4e13c7f86dd1537bb6b34c69e
ke-kaa <amsalukaku122@gmail.com>
2025-06-12 13:37:47 +0300

    Create TimesheetModel

--- api_timesheet/models.py
+++ api_timesheet/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
-# Create your models here.
+class TimesheetModel(models.Model):
+    """
+    Model to track employee timesheets, including clock-in and clock-out times,
+    and automatically calculates the total working hours for each shift.
+
+    Fields:
+        user (ForeignKey): Reference to the User who owns the timesheet entry.
+        clock_in_time (DateTimeField): The datetime when the employee clocks in.
+        clock_out_time (DateTimeField): The datetime when the employee clocks out.
+        working_hours (DurationField): The duration between clock-in and clock-out, auto-calculated.
+
+    Methods:
+        save(): Calculates and stores working_hours before saving.
+        clean(): Validates that clock_out_time is not before clock_in_time.
+
+    Meta:
+        Adds an index on user and clock_in_time for efficient querying.
+    """
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    clock_in_time = models.DateTimeField(null=True, blank=True,db_index=True)
+    clock_out_time = models.DateTimeField(null=True, blank=True, db_index=True)
+    working_hours = models.DurationField(null=True, blank=True, editable=False, db_index=True)
+
+    def save(self, *args, **kwargs):
+        if self.clock_in_time and self.clock_out_time:
+            self.working_hours = self.clock_out_time - self.clock_in_time    
+        else:
+            self.working_hours = None
+
+        super().save(*args, **kwargs)
+    
+    def clean(self):
+        if self.clock_out_time < self.clock_in_time:
+            raise ValidationError("Clock in time cannot be before Clock out time.")
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['user', 'clock_in_time'])
+        ]
