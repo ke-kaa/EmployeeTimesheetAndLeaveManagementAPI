2256eea64a3045a8f19b8e1d8aaa00753d279fca
ke-kaa <amsalukaku122@gmail.com>
2025-06-13 09:50:20 +0300

    Update settings to use JWT as auth-backend

--- EmployeeTimesheetAndLeaveManagement/settings.py
+++ EmployeeTimesheetAndLeaveManagement/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api_authentication',
+    'api_leave',
+    'api_timesheet',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
