503c7e87acd22ff89df565a43f4e696b105f5c00
ke-kaa <amsalukaku122@gmail.com>
2025-06-15 00:49:42 +0300

    Add url to access EmployeeCreationView

--- api_authentication/urls.py
+++ api_authentication/urls.py
@@ -3,8 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import views as my_views
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/create/account/', my_views.EmployeeCreationView.as_view(), name='account-create')
 ]
