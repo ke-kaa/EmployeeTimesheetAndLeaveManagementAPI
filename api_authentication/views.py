5b21dcd9fd97163ee3aeeb5067428a724d8ab251
ke-kaa <amsalukaku122@gmail.com>
2025-06-15 00:46:16 +0300

    Create EmployeeCreationView

--- api_authentication/views.py
+++ api_authentication/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .models import EmployeeModel
+from .serializers import EmployeeAccountCreationSerializer
+from .permissions import IsManager
+
+
+class EmployeeCreationView(generics.CreateAPIView):
+    serializer_class = EmployeeAccountCreationSerializer
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser | IsManager]
+    authentication_classes = [JWTAuthentication]
