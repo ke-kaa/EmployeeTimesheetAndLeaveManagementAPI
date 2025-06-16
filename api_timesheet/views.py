2d2fa1a1e69876e5c9f5aaca7ae9433d2814e3af
ke-kaa <amsalukaku122@gmail.com>
2025-06-17 01:02:11 +0300

    Create ClockInView

--- api_timesheet/views.py
+++ api_timesheet/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from . import serializers as my_serializers
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from datetime import datetime
+from rest_framework.utils import timezone
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+from . import models as my_models
+
+
+class ClockInView(generics.CreateAPIView):
+    serializer_class = my_serializers.ClockInSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        timesheet = serializer.save()
+
+        return Response({
+            'timesheet_id': timesheet.id,
+            'clock_in_time': timesheet.clock_in_time,
+        }, status=status.HTTP_201_CREATED)
