8e53bf3fa1dc628ca3017fea771305d821e81418
ke-kaa <amsalukaku122@gmail.com>
2025-06-17 02:12:53 +0300

    Create EmployeeTimesheetView

--- api_timesheet/views.py
+++ api_timesheet/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from . import serializers as my_serializers
-from rest_framework import generics
+from rest_framework import generics, filters, status
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+
 
 
 from . import models as my_models
@@ -26,7 +27,7 @@ class ClockInView(generics.CreateAPIView):
         }, status=status.HTTP_201_CREATED)
     
 
-class ClockOutTime(generics.UpdateAPIView):
+class ClockOutView(generics.UpdateAPIView):
     serializer_class = my_serializers.ClockOutSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -41,3 +42,17 @@ class ClockOutTime(generics.UpdateAPIView):
             'clock_in_time': timesheet.clock_in_time,
             'clock_out_time': timesheet.clock_out_time,
         }, status=status.HTTP_200_OK)
+    
+
+class EmployeeTimesheetView(generics.ListAPIView):
+    serializer_class = my_serializers.EmployeeTimesheet
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
+    page_size = 15
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['clock_in_time', 'clock_out_time', 'working_hours']
+    ordering = ['-clock_in_time', '-clock_out_time']
+    
+    def get_queryset(self):
+        return my_models.TimesheetModel.objects.filter(user=self.request.user)
