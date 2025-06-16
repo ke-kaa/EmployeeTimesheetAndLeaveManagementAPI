ee1d7a62c68983016e2b41bdca86db9364e991cc
ke-kaa <amsalukaku122@gmail.com>
2025-06-16 12:29:53 +0300

    Create url for EmployeeProfileRetrieveUpdateView

--- api_authentication/urls.py
+++ api_authentication/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views as my_views
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/create/account/', my_views.EmployeeCreationView.as_view(), name='account-create')
+    path('/token/', my_views.LoginView.as_view(), name='token-obtain-pair'),
+    path('/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('/create/account/', my_views.EmployeeCreationView.as_view(), name='account-create'),
+    path('/reset-initial-password/',my_views.InitialPasswordResetView.as_view(), name='password-reset'),
+    path('/employee/me/', my_views.EmployeeProfileRetrieveUpdateView.as_view(), name='employee-self-profile')
 ]
