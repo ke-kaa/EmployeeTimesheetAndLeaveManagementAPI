4891338c9bef9e16d63de5596142086b08c91108
ke-kaa <amsalukaku122@gmail.com>
2025-06-17 03:00:03 +0300

    Create IsManger permission

---
+++ api_timesheet/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and hasattr(request.user, 'employee')
+            and request.user.employee.role == 'MANAGER'
+            )
